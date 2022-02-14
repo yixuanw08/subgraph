d20991d44927b423d049d3f2685df35c8cd67cef
yixuanw08 <79735534+yixuanw08@users.noreply.github.com>
2022-02-14 10:30:50 +0800

    ad d

--- mapping/transfer.ts
+++ mapping/transfer.ts
@@ -2,23 +2,10 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { User } from "../generated/schema";
 import { Transfer } from "../generated/Transfer/USDT";
+// import { execute } from "../generated/Transfer/USDT";
 
-// export function handleTransfer(event: Transfer): void {
-//   let user = User.load(event.params.from.toHexString());//But before we create a new User object with the given address, we need to check if the user already exists.the load method accepts the unique ID for the entity.
-//   if (user == null) {
-//     user = new User(event.params.from.toHexString());
-//     user.totalTransfers = BigInt.fromI32(0);
-//     user.totalAmount = BigInt.fromI32(0);
-//   }
 
-//   user.totalTransfers = user.totalTransfers.plus(BigInt.fromI32(1));
-//   user.totalAmount = user.totalAmount.plus(event.params.value);
-
-//   user.save();
-
-// }
-
-const BI_ONE = BigInt.fromI32(1);
+// const BI_ONE = BigInt.fromI32(1);
 export function handleTransfer(event: Transfer): void {
   let transfer = User.load(event.transaction.hash.toHex());
   if (transfer == null) transfer = new User(event.transaction.hash.toHex());//if not existed create a new
