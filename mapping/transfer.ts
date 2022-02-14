8c2623068bbce3b823e026019504de2a8e783c9f
yixuanw08 <79735534+yixuanw08@users.noreply.github.com>
2022-02-14 10:37:53 +0800

    add

--- mapping/transfer.ts
+++ mapping/transfer.ts
@@ -1,11 +1,7 @@
 /* eslint-disable prefer-const */
-import { BigInt } from "@graphprotocol/graph-ts";
 import { User } from "../generated/schema";
 import { Transfer } from "../generated/Transfer/USDT";
-// import { execute } from "../generated/Transfer/USDT";
 
-
-// const BI_ONE = BigInt.fromI32(1);
 export function handleTransfer(event: Transfer): void {
   let transfer = User.load(event.transaction.hash.toHex());
   if (transfer == null) transfer = new User(event.transaction.hash.toHex());//if not existed create a new
